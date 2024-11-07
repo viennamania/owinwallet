287aec532478e9987095e8688c14b0b4d3258bad
seunghyonpark <creath.park@gmail.com>
2024-11-07 16:28:52 +0900

    Update agent API route to allow additional wallet address access and improve response handling

--- src/app/api/agent/getApplications/route.ts
+++ src/app/api/agent/getApplications/route.ts
@@ -20,19 +20,27 @@ export async function POST(request: NextRequest) {
     return NextResponse.error();
   }
 
-  if (walletAddress !== "0x7bfF3359841D26C8046364b93E7dA01886ae1c22"
-      && walletAddress !== "0xFb580c68794A963632FF272ab5A7233ee6114fef") {
+  if (walletAddress === "0x7bfF3359841D26C8046364b93E7dA01886ae1c22"
+      || walletAddress === "0xFb580c68794A963632FF272ab5A7233ee6114fef"
+      || walletAddress === "0x3e3E906e33D25cecA1aee550CACB1bCE74450Ed6"
+    
+    ) {
+
+      const result = await getAllAgents({});
+ 
+      return NextResponse.json({
+    
+        result,
+        
+      });
+
+
+  } else {
 
     return NextResponse.error();
 
   }
 
-  const result = await getAllAgents({});
- 
-  return NextResponse.json({
 
-    result,
-    
-  });
   
 }
