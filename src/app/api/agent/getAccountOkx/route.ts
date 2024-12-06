bbe7bbafb9bce328acf8a0887f4d3d60101f9fc7
seunghyonpark <creath.park@gmail.com>
2024-12-06 11:08:12 +0900

    Implement POST endpoint for account retrieval in getAccountOkx API

---
+++ src/app/api/agent/getAccountOkx/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import moment from 'moment';
+
+
+// var HmacSHA256 = require('crypto-js/hmac-sha256')
+
+import {
+  HmacSHA256,
+} from 'crypto-js';
+
+import CryptoJS from 'crypto-js';
+
+
+/*
+function sign_sha(method, baseurl, path, data) {
+    var pars = [];
+    for (let item in data) {
+        pars.push(item + "=" + encodeURIComponent(data[item]));
+    }
+    var p = pars.sort().join("&");
+    var meta = [method, baseurl, path, p].join('\n');
+    // console.log(meta);
+    var hash = HmacSHA256(meta, config.huobi.secretkey);
+    var Signature = encodeURIComponent(CryptoJS.enc.Base64.stringify(hash));
+    // console.log(`Signature: ${Signature}`);
+    p += `&Signature=${Signature}`;
+    // console.log(p);
+    return p;
+}
+*/
+
+
+
+
+
+export async function POST(request: NextRequest) {
+
+    const body = await request.json();
+
+  
+    const {
+        htxAccessKey,
+        htxSecretKey,
+    } = body;
+  
+
+    return NextResponse.json({
+        result: {
+            status: "ok",
+            data: {}
+        },
+    });
+
+}
