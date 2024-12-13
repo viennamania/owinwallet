ed4a9cbf59b8fa2f832a3ab08ab37bdb313906f5
seunghyonpark <creath.park@gmail.com>
2024-12-13 16:43:32 +0900

    Update wallet address checks in getApplications route

--- src/app/api/agent/getApplications/route.ts
+++ src/app/api/agent/getApplications/route.ts
@@ -35,13 +35,9 @@ export async function POST(request: NextRequest) {
 
 
       // AI Labs
-      || walletAddress === "0x40fa492D30815F17BD83B6945103AEfbe7B92EBe" // AI Labs
-      || walletAddress === "0x3e3E906e33D25cecA1aee550CACB1bCE74450Ed6"
-      || walletAddress === "0xE052B4f1B6842d422Debf17eB5361138830d6c1c"
-      || walletAddress === "0x78d937Ec95f1674BF9E43d90D6231f504AC2D6c3"
-
-      || walletAddress === "0xaB6fB9F80469511195Db7Ac707Cb3F4aD99b1755" // center
-
+      || walletAddress === "0x8b633C7273a373F04B1C167dd0781cd38c7EBAc8" // 허태인 
+      || walletAddress === "0xc05F8B05C1EC24AdB332B31c5A479EA6EC7A78BD" // 차보형 과장
+      
     
     ) {
 
