d759ecb800f0e996f174b32b903982b9f800da42
seunghyonpark <creath.park@gmail.com>
2025-01-13 12:27:41 +0900

    Add user name display to dashboard page component

--- src/app/[lang]/[chain]/dashboard/page.tsx
+++ src/app/[lang]/[chain]/dashboard/page.tsx
@@ -2788,6 +2788,16 @@ export default function AIPage({ params }: any) {
                                                 }
                                                 </div>
                                             </div>
+                                            
+                                            {/* userName */}
+                                            <div className='w-full flex flex-row items-center justify-between gap-2'>
+                                                <span className='text-xs font-semibold text-gray-800'>
+                                                    회워아이디
+                                                </span>
+                                                <span className='text-sm text-gray-800'>
+                                                    {application.userName}
+                                                </span>
+                                            </div>
                                             {/* wallet address */}
                                             <div className='w-full flex flex-row items-center justify-between gap-2'>
                                                 <span className='text-xs font-semibold text-gray-800'>
