9f0a4fac688db66795afafb1ed02addb850220de
seunghyonpark <creath.park@gmail.com>
2025-01-14 09:12:48 +0900

    Update trading volume threshold for settlement eligibility from 50,000 USDT to 10,000 USDT

--- src/app/[lang]/[chain]/dashboard/page.tsx
+++ src/app/[lang]/[chain]/dashboard/page.tsx
@@ -1895,13 +1895,13 @@ export default function AIPage({ params }: any) {
                                                     {/* dot */}
                                                     <div className='w-2 h-2 bg-red-500 rounded-full'></div>
                                                     <span className='flex text-sm text-gray-800'>
-                                                        정산할 거래량이 50,000 USDT 이상일 경우 정산할 수 있습니다.
+                                                        정산할 거래량이 10,000 USDT 이상일 경우 정산할 수 있습니다.
                                                     </span>
                                                 </div>
         
                                                 {
                                                     (application?.affiliateInvitee?.data?.volMonth ? Number(application.affiliateInvitee.data.volMonth
-                                                    - application?.claimedTradingVolume || 0) > 50000 : false) ? (
+                                                    - application?.claimedTradingVolume || 0) > 10000 : false) ? (
 
 
                                                     <button
